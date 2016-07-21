ad61e881b5f0eeb1b5225bf3275b6a5818ad27c6
Patrick Villanueva <villanuv@yahoo.com>
2016-07-21 13:13:53 -0500

    changed to private app keys

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,6 +1,6 @@
 ShopifyApp.configure do |config|
-  config.api_key = "6215253215274c21ed1df2f9069ebd29"
-  config.secret = "7af68a2c551f6e971be214b0ed7ba5a6"
+  config.api_key = "a21f1b98bad192f1098711f168cfd40c"
+  config.secret = "31e7bee6a4139bb64b4b67eb99cbb442"
   config.scope = "read_orders, read_products"
   config.embedded_app = true
 end
