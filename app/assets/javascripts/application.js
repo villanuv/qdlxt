ebfa4a232e8792c51edade1f9b60938c9da2f7a9
Patrick Villanueva <villanuv@yahoo.com>
2016-07-22 15:55:03 -0500

    more js testing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,8 +15,8 @@
 //= require_tree .
 
 function getProductInfo(productId){
-  var lcTag = $('"#" + productId + "-lc"').value;
-  var l4l4uTag = $('"#" + productId + "-l4l4u"').value;
+  var lcTag = $('#' + productId + '-lc').value;
+  var l4l4uTag = $('#' + productId + '-l4l4u').value;
   var prodString = productId + ' ' + lcTag + ' ' + l4l4uTag;
   alert(prodString);
 }
