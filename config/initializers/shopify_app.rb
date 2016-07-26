b83d9effd2840b22fa9b569a6edbcaaaa5fd2eef
Patrick Villanueva <villanuv@yahoo.com>
2016-07-26 12:51:49 -0500

    removed update products from config

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
   config.api_key = "6215253215274c21ed1df2f9069ebd29"
   config.secret = "7af68a2c551f6e971be214b0ed7ba5a6"
-  config.scope = "read_orders, read_products, update_products"
+  config.scope = "read_orders, read_products"
   config.embedded_app = true
   config.webhooks = [
     {topic: 'products/update', address: 'http://shopify-rails-test.herokuapp.com/webhooks/products_update', format: 'json'},
