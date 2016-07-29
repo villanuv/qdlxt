fa87885dbe864ba27b8c59c0ab4afa09080f4994
Patrick Villanueva <villanuv@yahoo.com>
2016-07-29 15:51:26 -0500

    changed scope setting

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,6 +1,6 @@
 ShopifyApp.configure do |config|
   config.api_key = "6215253215274c21ed1df2f9069ebd29"
   config.secret = "8a46886b598bd2ee60272afee5a1458a"
-  config.scope = "read_orders, read_products, write_products, update_products"
+  config.scope = "read_orders, read_products, write_products"
   config.embedded_app = true
 end
