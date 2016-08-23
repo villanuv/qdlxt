d3aaccf64677e4acf037bcc6d8a3f308ff196c96
Patrick Villanueva <villanuv@yahoo.com>
2016-08-23 14:23:31 -0500

    added note

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,4 +1,5 @@
 ShopifyApp.configure do |config|
+  # old data
   config.api_key = "6215253215274c21ed1df2f9069ebd29"
   config.secret = "36ac4aa77f64139fef666eb987a7d6e0"
   config.scope = "read_orders, read_products, write_products"
